c503c292428351741e46d932ba2590644ef4eb53
Banjer71 <davgio7@gmail.com>
2020-06-25 17:00:14 +0100

    added the old style ready to be refactored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
+import SongPage from "./components/SongPage";
+
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lyrics-Bites</h1>
+      <Router>
+        <Route path="/" exact component={SearchBar} />
+        <Route path="/SongPage" exact component={SongPage} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import ArtistCard from "./ArtistCard";
+import Header from "./Header";
+import Loader from "./Loader";
+import "./searchbar.css";
+
+const SearchBar = () => {
+  const [trackList, setTrackList] = useState([]);
+  const [trackTitle, setTrackTitle] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("q_artist");
+
+  const getTrack = async (e) => {
+    e.preventDefault();
+    const restUrl = `/track.search?${query}=${trackTitle}&page_size=4&page=1&f_has_lyrics=1&s_track_rating=desc&apikey=${process.env.REACT_APP_API_KEY_MUSICMATCH}`;
+    setIsLoading(true);
+    let response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1${restUrl}`
+    );
+    let data = await response.json();
+    let info = data.message.body.track_list;
+    setTrackList(info);
+    setIsLoading(false);
+    setTrackTitle("");
+  };
+
+  const handleChange = (e) => {
+    setTrackTitle(e.target.value);
+  };
+
+  const getLyricsSelected = (e) => {
+    const newValue = e.target.value;
+    if (newValue === "q_lyrics") {
+      setQuery(newValue);
+    } else if (newValue === "q_track") {
+      setQuery(newValue);
+    } else {
+      setQuery(newValue);
+    }
+  };
+
+  return (
+    <div className="search-bar">
+      <Header />
+      <div className="field">
+        <form className="form-u" onSubmit={getTrack}>
+          <label>Search a Song</label>
+          <select value={query} onChange={getLyricsSelected}>
+            <option value="q_artist">By Artist</option>
+            <option value="q_track">By Song</option>
+            <option value="q_lyrics">By Word</option>
+          </select>
+
+          <input
+            text="text"
+            name="trackTitle"
+            autoComplete="on"
+            placeholder="search..."
+            value={trackTitle}
+            onChange={handleChange}
+          />
+          <button type="submit">Get Songs</button>
+        </form>
+      </div>
+
+      <div className="grid track">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          trackList &&
+          trackList.map((name) => {
+            return <ArtistCard key={name.track.track_id} track={name.track} />;
+          })
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
